ddc23d21541943c737d8a913850812d83b65cf3f
Maksym Shalenyi <maksym.shalenyi@gmail.com>
2024-06-15 10:42:39 -0700

    initial commit

---
+++ gilded_rose.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+
+# rules = [
+# - Once the sell by date has passed, `Quality` degrades twice as fast
+# - The `Quality` of an item is never negative
+# - __"Aged Brie"__ actually increases in `Quality` the older it gets
+# - The `Quality` of an item is never more than `50`
+# - __"Sulfuras"__, being a legendary item, never has to be sold or decreases in `Quality`
+# - __"Backstage passes"__, like aged brie, increases in `Quality` as its `SellIn` value approaches;
+# 	- `Quality` increases by `2` when there are `10` days or less and by `3` when there are `5` days or less but
+# 	- `Quality` drops to `0` after the concert
+# ]
+
+
+
+class GildedRose(object):
+
+    def __init__(self, items):
+        self.items = items
+
+    def update_quality(self):
+        for item in self.items:
+            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+                if item.quality > 0:
+                    if item.name != "Sulfuras, Hand of Ragnaros":
+                        item.quality = item.quality - 1
+            else:
+                if item.quality < 50:
+                    item.quality = item.quality + 1
+                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                        if item.sell_in < 11:
+                            if item.quality < 50:
+                                item.quality = item.quality + 1
+                        if item.sell_in < 6:
+                            if item.quality < 50:
+                                item.quality = item.quality + 1
+            if item.name != "Sulfuras, Hand of Ragnaros":
+                item.sell_in = item.sell_in - 1
+            if item.sell_in < 0:
+                if item.name != "Aged Brie":
+                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
+                        if item.quality > 0:
+                            if item.name != "Sulfuras, Hand of Ragnaros":
+                                item.quality = item.quality - 1
+                    else:
+                        item.quality = item.quality - item.quality
+                else:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+
+
+class Item:
+    def __init__(self, name, sell_in, quality):
+        self.name = name
+        self.sell_in = sell_in
+        self.quality = quality
+
+    def __repr__(self):
+        return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
---
+++ start_texttest.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+texttest -d . -con "$@"
